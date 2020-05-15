f6bb771411fa7e76e370edb09464723b0aed85c2
Hirokazu Kiyomaru <h.kiyomaru@gmail.com>
2020-05-15 18:41:06 +0900

    move setup configuration to setup.cfg

--- pyknp_eventgraph/__init__.py
+++ pyknp_eventgraph/__init__.py
@@ -1,6 +1,6 @@
-from pyknp_eventgraph import _version
+from pathlib import Path
+__version__ = Path(__file__).parent.joinpath('VERSION').open().read().rstrip()
+
 from pyknp_eventgraph.eventgraph import EventGraph
 from pyknp_eventgraph.visualizer import EventGraphVisualizer
 from pyknp_eventgraph.visualizer import make_image
-
-__version__ = _version.__version__
--- setup.py
+++ setup.py
@@ -1,25 +1,2 @@
-#!/usr/bin/env python
-from setuptools import setup, find_packages
-import os
-
-here = os.path.abspath(os.path.dirname(__file__))
-exec(open(os.path.join(here, 'pyknp_eventgraph', '_version.py')).read())
-
-setup(
-    name='pyknp-eventgraph',
-    version=__version__,
-    description='Python module for building EventGraph from KNP result',
-    author='Kurohashi-Kawahara Lab, Kyoto University',
-    author_email='contact@nlp.ist.i.kyoto-u.ac.jp',
-    license='BSD-3-Clause',
-    packages=find_packages(exclude=('tests', 'docs')),
-    install_requires=['pyknp==0.4.1', 'graphviz==0.10.1'],
-    setup_requires=['pytest-runner'],
-    tests_require=['pytest', 'parameterized'],
-    entry_points={
-        'console_scripts': [
-            'evg=pyknp_eventgraph.cli:build_eventgraph',
-            'evgviz=pyknp_eventgraph.cli:visualize_eventgraph',
-        ],
-    }
-)
+from setuptools import setup
+setup()
