f51e1a1eb4ee419806dca42f03b5215d759f681e
Hirokazu Kiyomaru <h.kiyomaru@gmail.com>
2020-09-22 01:38:56 +0900

    update examples

--- examples/visualize_eventgraph.py
+++ examples/visualize_eventgraph.py
@@ -8,9 +8,16 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('IN', help='path to EventGraph')
     parser.add_argument('OUT', help='path to an output file')
+    parser.add_argument('--binary', action='store_true', help='use Python\'s pickle utility for serialization')
     args = parser.parse_args()
 
-    evg = EventGraph.load(args.IN)
+    if args.binary:
+        with open(args.IN, 'rb') as f:
+            evg = EventGraph.load(f, binary=args.binary)
+    else:
+        with open(args.IN) as f:
+            evg = EventGraph.load(f, binary=args.binary)
+
     make_image(evg, args.OUT)
 
 
