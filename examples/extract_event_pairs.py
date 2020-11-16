61dd0564063c009d97622c749a760f9e11d7e75d
Hirokazu Kiyomaru <h.kiyomaru@gmail.com>
2020-11-16 23:26:19 +0900

    update an example

--- examples/extract_event_pairs.py
+++ examples/extract_event_pairs.py
@@ -4,10 +4,6 @@ from pyknp_eventgraph import EventGraph
 from pyknp_eventgraph.utils import read_knp_result_file
 
 
-def print_event_pair(event_pair: dict) -> None:
-    print(event_pair['modifier_event'] + ' -({})-> '.format(event_pair['label']) + event_pair['head_event'])
-
-
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('IN', help='path to a knp result file')
@@ -16,15 +12,8 @@ def main():
     blists = read_knp_result_file(args.IN)
     evg = EventGraph.build(blists)
 
-    event_pairs = []
     for relation in evg.relations:
-        # If `include_modifiers` is true, modifiers' tokens will be included.
-        modifier = relation.modifier.surf_(include_modifiers=True)
-        head = relation.head.surf_(include_modifiers=True)
-        event_pairs.append({'modifier_event': modifier, 'head_event': head, 'label': relation.label})
-
-    for event_pair in event_pairs[:10]:
-        print_event_pair(event_pair)
+        print(relation.modifier.surf_with_mark + f' -({relation.label})-> ' + relation.head.surf_with_mark)
 
 
 if __name__ == '__main__':
